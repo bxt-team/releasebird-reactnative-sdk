0ebf1896d249e00f55149913a5e59b2b401d3227
Christian Zillmann <christian.zillmann>
2024-08-09 13:05:14 +0200

    init

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,20 @@
+export type ReleasebirdUserProperty = {
+    email?: string;
+    name?: string;
+    phone?: string;
+    value?: number;
+    sla?: number;
+    plan?: string;
+    companyName?: string;
+    companyId?: string;
+    customData?: {
+        [key: string]: string | number;
+    };
+};
+type ReleasebirdSdkType = {
+    initialize(token: string): void;
+    identify(userId: string, userProperties: ReleasebirdUserProperty): void;
+};
+declare const _default: ReleasebirdSdkType;
+export default _default;
+//# sourceMappingURL=index.d.ts.map
---
+++ src/index.tsx
@@ -0,0 +1,37 @@
+import {NativeModules, NativeEventEmitter, Platform} from 'react-native';
+
+const LINKING_ERROR =
+    `The package 'releasebird-reactnative-sdk' doesn't seem to be linked. Make sure: \n\n` +
+    Platform.select({ios: "- You have run 'pod install'\n", default: ''}) +
+    '- You rebuilt the app after installing the package\n' +
+    '- You are not using Expo managed workflow\n';
+
+export type ReleasebirdUserProperty = {
+    email?: string;
+    name?: string;
+    phone?: string;
+    value?: number;
+    sla?: number;
+    plan?: string;
+    companyName?: string;
+    companyId?: string;
+    customData?: { [key: string]: string | number };
+};
+
+type ReleasebirdSdkType = {
+    initialize(token: string): void;
+    identify(userId: string, userProperties: ReleasebirdUserProperty): void;
+};
+
+const ReleasebirdSdk = NativeModules.Gleapsdk
+    ? NativeModules.Gleapsdk
+    : new Proxy(
+        {},
+        {
+            get() {
+                throw new Error(LINKING_ERROR);
+            },
+        }
+    );
+
+export default ReleasebirdSdk as ReleasebirdSdkType;
