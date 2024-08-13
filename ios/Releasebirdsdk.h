a61e03fcc759f18ff646b91390f369ff792a2c66
Christian Zillmann <christian.zillmann>
2024-08-13 11:12:41 +0200

    init

---
+++ ios/Releasebirdsdk.h
@@ -0,0 +1,7 @@
+#import <React/RCTBridgeModule.h>
+#import <releasebird-ios-sdk/Relasebird.h>
+#import <React/RCTEventEmitter.h>
+
+@interface Releasebirdsdk : RCTEventEmitter <RCTBridgeModule, ReleasebirdDelegate>
+
+@end
--- ios/Releasebirdsdk.m
+++ ios/Releasebirdsdk.m
@@ -14,72 +14,30 @@ static NSString *const RCTShowDevMenuNotification = @"RCTShowDevMenuNotification
 RCT_EXPORT_MODULE()
 
 - (void)initSDK {
-    Gleap.sharedInstance.delegate = self;
-    [Gleap setApplicationType: REACTNATIVE];
 }
 
 RCT_EXPORT_METHOD(initialize:(NSString *)token)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self initSDK];
-        [Gleap setAutoActivationMethodsDisabled];
-        [Gleap initializeWithToken: token];
-        [Gleap trackEvent: @"pageView" withData: @{
-            @"page": @"MainPage"
-        }];
+
     });
 }
 
 - (void)initialized {
-    if (_hasListeners) {
-        [self sendEventWithName:@"initialized" body: @{}];
-    }
+
 }
 
 RCT_EXPORT_METHOD(initialize:(NSString *)token)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        [self initSDK];
-        [Gleap setAutoActivationMethodsDisabled];
-        [Gleap initializeWithToken: token];
-        [Gleap trackEvent: @"pageView" withData: @{
-            @"page": @"MainPage"
-        }];
+
     });
 }
 
 RCT_EXPORT_METHOD(identify:(NSString *)userId withUserProperties: (NSDictionary *)userProperties)
 {
     dispatch_async(dispatch_get_main_queue(), ^{
-        GleapUserProperty *userProperty = [[GleapUserProperty alloc] init];
-        if (userProperties != nil && [userProperties objectForKey: @"name"] != nil) {
-            userProperty.name = [userProperties objectForKey: @"name"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"email"] != nil) {
-            userProperty.email = [userProperties objectForKey: @"email"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"phone"] != nil) {
-            userProperty.phone = [userProperties objectForKey: @"phone"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"value"] != nil) {
-            userProperty.value = [userProperties objectForKey: @"value"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"sla"] != nil) {
-            userProperty.sla = [userProperties objectForKey: @"sla"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"plan"] != nil) {
-            userProperty.plan = [userProperties objectForKey: @"plan"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"companyName"] != nil) {
-            userProperty.companyName = [userProperties objectForKey: @"companyName"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"companyId"] != nil) {
-            userProperty.companyId = [userProperties objectForKey: @"companyId"];
-        }
-        if (userProperties != nil && [userProperties objectForKey: @"customData"] != nil) {
-            userProperty.customData = [userProperties objectForKey: @"customData"];
-        }
-        [Gleap identifyContact: userId andData: userProperty];
+
     });
 }
 
@@ -87,7 +45,6 @@ RCT_EXPORT_METHOD(identify:(NSString *)userId withUserProperties: (NSDictionary 
 - (void)dealloc
 {
     @try{
-       [[NSNotificationCenter defaultCenter] removeObserver: self];
     } @catch(id anException) {}
 }
 
